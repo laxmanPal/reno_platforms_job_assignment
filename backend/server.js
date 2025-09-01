1d0c859d00485e2202aa79a697eecd465c3f5217
laxmanPal <laxi27902@gmail.com>
2025-09-01 12:07:58 +0530

    added port

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,11 @@ import schoolsRouter from "./router.js";
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(cors())
 app.use(express.json());
 app.use("/schoolImages", express.static("schoolImages"));
 app.use("/api/", schoolsRouter);
 
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
